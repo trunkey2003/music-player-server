696e941016ed13790a4a8d260cb2fd72d585727a
trunkey2007 <hoquanglam090@gmail.com>
2021-12-12 19:02:20 +0700

    update sign up route

--- app/controllers/models/user.model.js
+++ app/controllers/models/user.model.js
@@ -6,8 +6,9 @@ const user = new Schema({
     dateOfBirth: {type:String, default:"01/01/2022"},
     Phone:{type:String, default:"0123456789"},
     Email:{type:String, required: true},
-    userid:{type:String},
-    username:{type:String, required: true},
+    userid:{type:String, unique: true},
+    username:{type:String, unique: true, required: true},
+    avatar:{type: String, default:"https://trunkey2003.github.io/general-img/default-profile-pic.jpg"},
     password:{type:String},
 });
 
